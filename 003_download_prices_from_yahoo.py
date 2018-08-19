74f7a350df161ee7d7a8069b74f8b096db783296
Xi Chen <chenxi0329@gmail.com>
2018-08-19 00:37:16 -0400

    finish script 003

--- 003_download_prices_from_yahoo.py
+++ 003_download_prices_from_yahoo.py
@@ -2,10 +2,11 @@ import psycopg2
 import os, time, subprocess
 
 conn_string = "host='192.168.99.100' dbname='alpha' user='postgres' password='password'"
-select_records_query = 'SELECT * FROM tickers'
+query_symbols = 'SELECT * FROM tickers'
 sleep_time_in_second = 1
+wait_between_download = False
 
-def requestCursor(conn_string):
+def request_cursor(conn_string):
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
     return cursor
@@ -19,13 +20,24 @@ def download(ticker):
     print(ticker)
     subprocess.call(['sh', 'get-yahoo-quotes.sh', ticker])
 
-cursor = requestCursor(conn_string)
-records = execute_read_query(select_records_query, cursor)
+def is_file_exist_in_current_folder(file_name):
+    return os.path.exists(file_name)
 
+cursor = request_cursor(conn_string)
+records = execute_read_query(query_symbols, cursor)
 tickers = [record[1] for record in records]
+failedList = []
 
 for ticker in tickers:
-    while not os.path.exists(ticker + '.' + 'csv'):
+    failedTimes = 0
+    while not is_file_exist_in_current_folder(ticker + '.' + 'csv'):
         download(ticker)
         sleep_time_in_second *= 2
-        time.sleep(sleep_time_in_second)
+        if wait_between_download:
+            time.sleep(sleep_time_in_second)
+        failedTimes += 1
+        if failedTimes > 5:
+            failedList.append(ticker)
+            break
+
+print(failedList)
